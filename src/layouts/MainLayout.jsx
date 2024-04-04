0392aa819baaf7fa8129bc56646d98ce56d8698d
Leonid <leosb@mail.ru>
2024-04-04 19:28:00 +0300

    Final result

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MainLayout = () => {
-  return <>
-  <NavBar />
-  <Outlet />
-  </>;
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <ToastContainer />
+    </>
+  );
 };
 
 export default MainLayout;
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 // import { useEffect, useState } from "react";
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 // import Spinner from "../components/Spinner";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const Jobpage = () => {
+const Jobpage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm("Are you sure to delete the job?");
+    if (!confirm) {
+      return;
+    }
+    deleteJob(jobId);
+    toast.success("Job deleted successfully");
+    navigate("/jobs");
+  };
   // const [job, SetJob] = useState(null);
   // const [loading, setLoading] = useState(true);
   // useEffect(() => {
@@ -85,18 +96,23 @@ const Jobpage = () => {
 
                 <h3 className="text-xl">Contact Phone:</h3>
 
-                <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactPhone}
+                </p>
               </div>
 
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/jobs/edit/${job.id}`}
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
